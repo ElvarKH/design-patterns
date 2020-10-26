a7548f95afa13bd8fc545bae5eeb19ba59f1e2b7
3lvar-khudiev <elvarkhudiev@gmail.com>
2020-10-26 18:23:30 +0400

    changes

--- src/main/java/creational_patterns/builder/builder_3/TestBuilder.java
+++ src/main/java/creational_patterns/builder/builder_3/TestBuilder.java
@@ -3,18 +3,20 @@ package creational_patterns.builder.builder_3;
 public class TestBuilder {
     public static void main(String[] args) {
 
+        // Create Director
         Waiter waiter = new Waiter();
 
+        // Create Builders
         HawaiianPizzaBuilder hawaiianPizzaBuilder = new HawaiianPizzaBuilder();
         SpicyPizzaBuilder spicyPizzaBuilder = new SpicyPizzaBuilder();
 
         // Construct and get hawaiian pizza
-        waiter.setPizzaBuilder(hawaiianPizzaBuilder);
+        waiter.setPizzaBuilder(hawaiianPizzaBuilder); // set hawaiian builder to director
         waiter.constructPizza();
         Pizza hawaiianPizza = waiter.getPizza();
 
         // Construct and get spicy pizza
-        waiter.setPizzaBuilder(spicyPizzaBuilder);
+        waiter.setPizzaBuilder(spicyPizzaBuilder);  // set hawaiian builder to director
         waiter.constructPizza();
         Pizza spicyPizza = waiter.getPizza();
 
